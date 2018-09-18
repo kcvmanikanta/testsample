7deceffdc5a0523a41dc05037762b82e9971732c
MANI <kchaitanya.nani@gmail.com>
2018-09-17 19:46:23 -0700

    test

--- src/main/java/org/openshift/quickstarts/undertow/servlet/test.java
+++ src/main/java/org/openshift/quickstarts/undertow/servlet/test.java
@@ -1,4 +1,11 @@
 package org.openshift.quickstarts.undertow.servlet;
 
+import edu.stanford.nlp.pipeline.StanfordCoreNLPServer;
+
+import java.io.IOException;
+
 public class test {
+    public static void main(String[] args) throws IOException {
+        StanfordCoreNLPServer.main(args);
+    }
 }
